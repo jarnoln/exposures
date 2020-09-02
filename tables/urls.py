8dfc500bd6ce8448f528ef924a3f8592979d90c1
Jarno Luoma-Nirva <jarno.luoma-nirva@vaisto.io>
2020-09-02 20:32:06 +0300

    Change tab ordering

--- tables/urls.py
+++ tables/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('all/', views.all_exposures, name='all_exposures'),
     path('municipalities/', views.exposures_by_municipality, name='exposures_by_municipality'),
-    path('categories/', views.exposures_by_category, name='exposures_by_category'),
-    path('', views.all_exposures, name='all_exposures'),
+    path('', views.exposures_by_category, name='exposures_by_category'),
 ]

d9d6f27c744420e24012c5c364ce8e81de85d44a
Jarno Luoma-Nirva <jarno.luoma-nirva@vaisto.io>
2020-09-11 18:58:59 +0300

    Add task for taking DB backups

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,6 @@
 # Very simple deploy script using Fabric 2.x
+import datetime
+
 from fabric import task
 
 @task
@@ -6,3 +8,12 @@ def deploy(c):
     source_directory = '/home/{}/sites/exposures/source'.format(c.user)
     c.run('cd {} && git pull'.format(source_directory))
     c.run('. {}/reset-gunicorn.sh'.format(source_directory))
+
+@task
+def backup(c):
+    db_file_path = '/home/{}/sites/exposures/source/db.sqlite3'.format(c.user)
+    now = datetime.datetime.now()
+    local_name = now.strftime('%Y-%m-%d_%H_%M_db.sqlite3')
+    print(local_name)
+    result = c.get(db_file_path, local=local_name)
+    print('Copied {} from {} to {}'.format(db_file_path, result.remote, result.local))

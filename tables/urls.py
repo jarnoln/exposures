9f39b19c58dbd318077c1c7f1ca46fd5d21809c8
Jarno Luoma-Nirva <jln@iki.fi>
2020-09-28 20:15:32 +0300

    Make exposures by date the default view

--- tables/urls.py
+++ tables/urls.py
@@ -5,10 +5,10 @@ from . import views
 urlpatterns = [
     path('api/exposures_by_date/', views.api_exposures_by_date, name='api_exposures_by_date'),
     path('exposure/<int:exposure_id>/', views.exposure_detail, name='exposure_detail'),
+    path('exposure/add/', views.exposure_edit, name='add_exposure'),
+    path('alerts/', views.alert_list, name='alert_list'),
     path('all/', views.all_exposures, name='all_exposures'),
-    path('dates/', views.exposures_by_date, name='exposures_by_date'),
     path('municipalities/', views.exposures_by_municipality, name='exposures_by_municipality'),
     path('categories/', views.exposures_by_category, name='exposures_by_category'),
-    path('exposure/add/', views.exposure_edit, name='add_exposure'),
-    path('', views.alert_list, name='alert_list'),
+    path('', views.exposures_by_date, name='exposures_by_date'),
 ]

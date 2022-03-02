90d1803efe9d28c06d133f9d9ce2f8693f089bd8
Jarno Luoma-Nirva <jln@iki.fi>
2022-03-02 22:41:48 +0200

    Update reset gunicorn script location

--- fabfile.py
+++ fabfile.py
@@ -7,7 +7,7 @@ from fabric import task
 def deploy(c):
     source_directory = '/home/{}/sites/exposures/source'.format(c.user)
     c.run('cd {} && git pull'.format(source_directory))
-    c.run('. {}/reset-gunicorn.sh'.format(source_directory))
+    c.run('. {}/deploy/reset-gunicorn.sh'.format(source_directory))
 
 @task
 def backup(c):
